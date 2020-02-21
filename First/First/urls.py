29a0749c83c63bd63424da8b3f7e0fd1f050d8b5
Advitya sharma <advityasharma21@gmail.com>
2020-02-21 14:38:07 +0530

    adding new contact

--- First/First/urls.py
+++ First/First/urls.py
@@ -19,5 +19,6 @@ from pages import views
 
 urlpatterns = [
     path('',views.home_view,name='home'),
+    path('contact/',views.contact_view,name='home'),
     path('admin/', admin.site.urls)
 ]
--- First/pages/views.py
+++ First/pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home_view(*args,**kwargs):
     return HttpResponse("<h1>Hello World</h1>")
+
+def contact_view(*args,**kwargs):
+    return HttpResponse('<h1>Contact paage</h1>')
