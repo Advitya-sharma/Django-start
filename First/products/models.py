efabca9dddb81b804d48f5041679fd2ad8a52bb7
Advitya sharma <advityasharma21@gmail.com>
2020-02-21 08:03:36 +0530

    hello world

--- First/products/models.py
+++ First/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.TextField(default="book")
-    price = models.TextField(default="5000")
-    description = models.TextField(default="great product")
+    name = models.CharField(max_length=1000)
+    price = models.DecimalField(decimal_places=3,max_digits=1000)
+    description = models.TextField()
