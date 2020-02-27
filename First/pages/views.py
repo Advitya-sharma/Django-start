9be72c3c914d89be54e60860d559fa12c6063beb
Advitya sharma <advityasharma21@gmail.com>
2020-02-27 17:10:53 +0530

    adding conditions

--- First/pages/views.py
+++ First/pages/views.py
@@ -12,8 +12,10 @@ def contact_view(request,*args,**kwargs):
     return render(request,"contact.html",{})
 
 def about_view(request,*args,**kwargs):
-    my_context ={}
-    return render(request,"about.html",{})
+    my_context ={"my_text" : "This is about us",
+                "my_number" : 123,
+                "my_list" : [1,2,3,4,5]}
+    return render(request,"about.html",my_context)
 
 def social_view(request,*args,**kwargs):
     return HttpResponse('<h1>Social page</h1>')
