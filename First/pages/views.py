0852ee5f563ec36c654a23826b08c97cb06994ae
Advitya sharma <advityasharma21@gmail.com>
2020-02-27 16:43:25 +0530

    adding blocks

--- First/pages/views.py
+++ First/pages/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home_view(*args,**kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request,*args,**kwargs):
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>")
+    return render(request,"home.html",{})
 
-def contact_view(*args,**kwargs):
-    return HttpResponse('<h1>Contact page</h1>')
 
-def about_view(*args,**kwargs):
-    return HttpResponse('<h1>About page</h1>')
+def contact_view(request,*args,**kwargs):
+    return render(request,"contact.html",{})
 
-def social_view(*args,**kwargs):
+def about_view(request,*args,**kwargs):
+    my_context ={}
+    return render(request,"about.html",{})
+
+def social_view(request,*args,**kwargs):
     return HttpResponse('<h1>Social page</h1>')
 
